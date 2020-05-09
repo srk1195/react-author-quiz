fea40b20d70ad5cf695c2941635f5479ba1f8a10
Rahul Kodumuru <ksairahul15@gmail.com>
2020-05-09 12:21:36 +0530

    addAuthormForm to redux
    Now, addAuthorForm component gets data from the redux store.

--- src/index.js
+++ src/index.js
@@ -120,37 +120,29 @@ function reducer(
 				highlight: "",
 				turnData: getTurnData(state.authors)
 			});
-
+		case "ADD_AUTHOR":
+			return Object.assign({}, state, {
+				authors: state.authors.concat([action.author])
+			});
 		default:
 			return state;
 	}
 }
 
-let store = Redux.createStore(reducer);
-
-function App() {
-	return (
-		<ReactRedux.Provider store={store}>
-			<AuthorQuiz />
-		</ReactRedux.Provider>
-	);
-}
-
-const AuhtorWrapper = withRouter(({ history }) => (
-	<AddAuthorForm
-		onAddAuthor={author => {
-			authors.push(author);
-			history.push("/");
-		}}
-	/>
-));
+// store creation.
+let store = Redux.createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
 	<BrowserRouter>
-		<React.Fragment>
-			<Route exact path="/" component={App} />
-			<Route exact path="/add" component={AuhtorWrapper} />
-		</React.Fragment>
+		<ReactRedux.Provider store={store}>
+			<React.Fragment>
+				<Route exact path="/" component={AuthorQuiz} />
+				<Route exact path="/add" component={AddAuthorForm} />
+			</React.Fragment>
+		</ReactRedux.Provider>
 	</BrowserRouter>,
 	document.getElementById("root")
 );
